14325fe5329626c97dc6801ec83fcb30d3de2311
SuTo <mrmosssir@gmail.com>
2026-01-02 16:27:38 +0800

    modify base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/gemini-3-stock-game-try",
   plugins: [
     react(),
     tailwindcss(),
